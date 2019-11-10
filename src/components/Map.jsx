34ea280d683600b6397b9da822cccd0bdd57408b
Kenneth Trang <kennethtrang@Kenneths-MacBook-Pro-1322.local>
2019-11-09 23:17:15 -0800

    added dynamic map which centers on input location or current location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 
 const Map = (props) => {
+
   return (
     <div>
       Map
+    <div>
+      <iframe width="500" height="300" src={`https://api.maptiler.com/maps/basic/?key=OeKji8TvwQYbzy8G5Pda#11/${props.latitude}/${props.longitude}/`}></iframe>
+    </div>
     </div>
   )
 }
